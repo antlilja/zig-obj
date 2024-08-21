876c85d2eb907a407314774c1bf083a832c82253
antlilja <liljaanton2001@gmail.com>
2024-08-21 20:26:31 +0200

    Update build.zig to 0.13

--- build.zig
+++ build.zig
@@ -1,7 +1,13 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("zig-obj", .{
-        .source_file = .{ .path = "src/main.zig" },
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const module = b.addModule("zig-obj", .{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
+    _ = module;
 }
